c884f9b0963c49e09448ff394ac84f4aaa7b458a
pevanget <84782645+pevanget@users.noreply.github.com>
2023-04-24 14:46:18 +0300

    Update TESTASCII.cs

--- Trigonix/Assets/TESTASCII.cs
+++ Trigonix/Assets/TESTASCII.cs
@@ -11,7 +11,8 @@ public class TESTASCII : MonoBehaviour
 
     private byte[] _encodedBytes;
     private string _decodedBytesASCII;
-    public bool Check = false;
+    public bool CheckValid = false;
+    public int MaxSizeForString = 10;
 
 
     // Start is called before the first frame update
@@ -21,32 +22,65 @@ public class TESTASCII : MonoBehaviour
         Debug.Log(_encodedBytes);
         foreach (byte b in _encodedBytes)
         {
-            Debug.Log(b + " ");
+            //Debug.Log(b + " ");
         }
         _decodedBytesASCII = ascii.GetString(_encodedBytes);
-        Debug.Log("ASCII text: " + _decodedBytesASCII);
-        StringContainsOnlyASCII(StringToEncode);
+        //Debug.Log("ASCII text: " + _decodedBytesASCII);
+        //StringContainsOnlyASCII(StringToEncode);
+        CheckStringValid(StringToEncode);
 
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Check)
+
+        if (CheckValid)
         {
-            Check = false;
-            StringContainsOnlyASCII(StringToEncode);
+            CheckValid = false;
+            CheckStringValid(StringToEncode);
         }
     }
 
-    [ContextMenu("StringContainsOnlyASCII")]
-    public bool StringContainsOnlyASCII(string stringToCheck)
+    private bool CheckStringValid(string stringToCheck)
+    {
+        bool IsNotNull = CheckNull(stringToCheck);
+        bool IsASCII = CheckStringContainsOnlyASCII(stringToCheck);
+        bool IsNotTooBig = CheckSize(stringToCheck);
+        bool isValid = (IsASCII && IsNotTooBig && IsNotNull);
+
+        if (isValid) Debug.Log("Valid string");
+        else Debug.LogWarning("Invalid string");
+        return isValid;
+    }
+
+    private bool CheckNull(string stringToCheck)
+    {
+        bool notNull = stringToCheck != null;
+        if (notNull) Debug.Log("String is not null");
+        else Debug.LogWarning("String is null!");
+        return notNull;
+    }
+    private bool CheckSize(string stringToCheck)
+    {
+        bool isNotTooBig = (stringToCheck.Length <= MaxSizeForString);
+        bool isNotEmpty = (stringToCheck.Length > 0);
+        bool isValidSize = (isNotEmpty && isNotTooBig);
+        if (isNotTooBig && isNotEmpty) Debug.Log("String is valid size");
+        else if (!isNotTooBig) Debug.LogWarning("String is too big!");
+        else if (!isNotEmpty) Debug.LogWarning("String is empty!");
+
+        return (isValidSize);
+    }
+
+
+    public bool CheckStringContainsOnlyASCII(string stringToCheck)
     {
         byte[] encodedBytesASCII = ascii.GetBytes(stringToCheck);
         string decodedBytesASCII = ascii.GetString(encodedBytesASCII);
         bool isASCII = (stringToCheck == decodedBytesASCII);
         if (isASCII) Debug.Log("This is an ASCII only string");
-        else Debug.Log("This is not an ASCII only string");
+        else Debug.LogWarning("This is not an ASCII only string");
         return (isASCII);
     }
 }
