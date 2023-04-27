ab44fe7819c93a832d38478f2720db993dd835ed
pevanget <84782645+pevanget@users.noreply.github.com>
2023-04-27 13:54:51 +0300

    dsadsa

---
+++ Trigonix/Assets/Scripts/ASCIIToElement.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Text;
+
+public class ASCIIToElement : MonoBehaviour
+{
+    //
+    byte value = 255;
+}
