a971961ff50293b4b3e9a1bba655c4d4ebad122d
pevanget <84782645+pevanget@users.noreply.github.com>
2023-05-10 13:18:16 +0300

    DECOOOOOODE

--- Trigonix/Assets/Scripts/Decoder.cs
+++ Trigonix/Assets/Scripts/Decoder.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class Decoder : MonoBehaviour
 {
+    [SerializeField] private CodeGenerator _codeGen;
     // Start is called before the first frame update
     void Start()
     {
@@ -18,6 +19,44 @@ public class Decoder : MonoBehaviour
 
     public void StartDecode()
     {
+        
+        
         Debug.Log("Starting Decoding");
+        Transform[] elements = _codeGen.GetElementsTransforms();
+        char[] characters = new char[elements.Length];
+        //Debug.Log(elements.Length);
+        for (int i = 0; i < elements.Length; i++)
+        {
+            characters[i] = DecodeChar(elements[i]);
+            //str = str + seed[i].ToString();
+            
+        }
+        string str = new string(characters);
+        Debug.Log(str);
+
+
+    }
+
+    private char DecodeChar(Transform el)
+    {
+        SpriteRenderer SR = el.GetComponent<SpriteRenderer>();
+        if (SR == null) return 'e';
+        else
+        {
+            return DecodeColor(SR.color);
+        }
+    }
+
+    private char DecodeColor(Color col)
+    {
+        float red = col.r * 3;
+        float green = col.g * 7;
+        float blue = col.b * 3;
+        float num = red * 32 + green * 4 + blue;
+        int numm = (int)num;
+        char myChar;
+        myChar = (char)numm;
+        //myChar = Convert.ToChar(numm);
+        return myChar;
     }
 }
