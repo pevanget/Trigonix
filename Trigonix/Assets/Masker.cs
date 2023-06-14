ef244811e0ec3dff64d92e73b349ed35c4d19628
pevanget <84782645+pevanget@users.noreply.github.com>
2023-06-14 18:10:55 +0300

    Update Masker.cs

--- Trigonix/Assets/Masker.cs
+++ Trigonix/Assets/Masker.cs
@@ -11,12 +11,73 @@ public class Masker : MonoBehaviour
 
     [SerializeField] private Encoder _encoder;
     private Transform[] _elements;
-    
+    private Transform[] _parentMasked = new Transform[3];
+    private Vector2Int[] _triangleCoordsElements = new Vector2Int[900];
+
+    private void Start()
+    {
+        for (int i = 0; i < 900; i++)
+        {
+            _triangleCoordsElements[i] = ToTriangleCoordinates(i);
+        }
+        //Debug.Log(_triangleCoordsElements[72]);
+        //Debug.Log(_triangleCoordsElements[772]);
+    }
 
     public void StartMasking()
     {
         _elements = _encoder.GetElementsTransforms();
+        ClearOldParentObjects();
+        for (int i = 0; i < 3; i++)
+        {
+            Vector3 positionToSpawn = new Vector3((-i - 1) * 50, 0, 0);
+            Debug.Log(i);
+            Debug.Log(_parentMasked.Length);
+            //Debug.Log(_parentMasked[i].position);
+            _parentMasked[i] = Instantiate(_elements[0].parent, positionToSpawn, Quaternion.identity);
+            _parentMasked[i].position = positionToSpawn;
+
+        }
         //Debug.Log(_elements.Length);
     }
 
+    private void ClearOldParentObjects()
+    {
+        for (int i = 0; i < 3; i++)
+        {
+            if (_parentMasked[i]!=null)
+            {
+                Destroy(_parentMasked[i].gameObject);
+            }
+        }
+    }
+
+    private void Mask1()
+    {
+
+    }
+    private void Mask2()
+    {
+
+    }
+    private void Mask3()
+    {
+
+    }
+    private void Mask4()
+    {
+
+    }
+
+    private Vector2Int ToTriangleCoordinates(int i)
+    {
+        int row = Mathf.FloorToInt(Mathf.Sqrt(i));
+        int numberInRow = i - (int)Mathf.Pow(row, 2);
+        Vector2Int triangleCoordinates = new(row, numberInRow);
+        return triangleCoordinates;
+    }
+
+
+    //find row = square root of i floored down
+    //find number in row = i - row^2
 }
