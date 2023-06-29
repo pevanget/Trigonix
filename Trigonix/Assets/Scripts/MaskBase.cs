13c3c978cc03c0fb822408ecaa5b6cb9b66af339
pevanget <84782645+pevanget@users.noreply.github.com>
2023-06-29 17:57:53 +0300

    maskaras

--- Trigonix/Assets/Scripts/MaskBase.cs
+++ Trigonix/Assets/Scripts/MaskBase.cs
@@ -4,9 +4,14 @@ public abstract class MaskBase : MonoBehaviour
 {
     public enum MaskName { mask1, mask2, mask3, mask4 };
     public MaskName maskName;
-
+    private float _thresholdColorComparison = 0.25f;
     protected Color[] _maskColors = new Color[MaskName.GetNames(typeof(MaskName)).Length];
     protected Color _thisMaskColor;
+    protected Vector2Int[] _triangleCoordinates = new Vector2Int[900];
+    protected int[] _leftNeighbor = new int[900];
+    protected int[] _rightNeighbor = new int[900];
+    protected int[] _upNeighbor = new int[900];
+    //protected int[] _downNeighbor = new int[900];
     //protected Transform _parentTrans;
 
     //allocate e.g. int maskFirstElements = 3  ===> start for i = 2. and do i =0 - 1 manually
@@ -46,12 +51,32 @@ public abstract class MaskBase : MonoBehaviour
         int scoreA = 0;
         for (int i = 0; i < elements.Length; i++)
         {
+            SpriteRenderer SR = elements[i].GetComponent<SpriteRenderer>();
+            //bool rightNeighborExists = CheckRightNeighborExists();
+            if (_leftNeighbor[i] >= 0)
+            {
+                //Debug.Log(i);
+                //Debug.Log(_leftNeighbor[i]);
+                //Debug
+                SpriteRenderer SRLeft = elements[_leftNeighbor[i]].GetComponent<SpriteRenderer>();
+                scoreA += CompareColors(SR.color, SRLeft.color);
+            }
+            if (_rightNeighbor[i]>=0)
+            {
+                SpriteRenderer SRRight = elements[_rightNeighbor[i]].GetComponent<SpriteRenderer>();
+                scoreA += CompareColors(SR.color, SRRight.color);
+            }
+            if (_upNeighbor[i] >= 0)
+            {
+                SpriteRenderer SRUp = elements[_upNeighbor[i]].GetComponent<SpriteRenderer>();
+                scoreA += CompareColors(SR.color, SRUp.color);
+            }
 
         }
 
 
 
-
+        Debug.Log(this + " mask has " + scoreA + " penalty score");
         return scoreA;
     }
 
@@ -59,6 +84,33 @@ public abstract class MaskBase : MonoBehaviour
     {
         return _maskColors;
     }    
+
+    public void SetTriangleCoords(Vector2Int[] triangleCoords)
+    {
+        _triangleCoordinates = triangleCoords;
+    }
+
+    public void SetNeighbors(int[] left, int[] right, int[] up)
+    {
+        _leftNeighbor = left;
+        _rightNeighbor = right;
+        _upNeighbor = up;
+        //_downNeighbor = down;
+    }
+
+
+    protected int CompareColors (Color a, Color b)
+    {
+        //Debug.Log(a);
+        //Debug.Log(b);
+        int valueToReturn = 0;
+        Color c = a - b;
+        float valueToComp = c.r * c.r + c.g * c.g + c.b * c.b;
+        if (valueToComp < _thresholdColorComparison) valueToReturn -= 1;
+        //else valueToReturn++;
+
+        return valueToReturn;
+    }
 
 
     
--- Trigonix/Assets/Scripts/Masker.cs
+++ Trigonix/Assets/Scripts/Masker.cs
@@ -16,21 +16,117 @@ public class Masker : MonoBehaviour
     private Transform[] _elements;
     private Transform[] _parentMasked;
     private Vector2Int[] _triangleCoordsElements = new Vector2Int[900];
+    private int[] _leftNeighbor = new int[900];
+    private int[] _rightNeighbor = new int[900];
+    private int[] _upNeighbor = new int[900];
+    //private int[] _downNeighbor = new int[900];
 
     private void Start()
     {
         for (int i = 0; i < 900; i++)
         {
             _triangleCoordsElements[i] = ToTriangleCoordinates(i);
+
         }
+
+        for (int i = 0; i < 900; i++)
+        {
+            FindNeighbors(i);
+        }
+
         int childCounter = 0;
         foreach (Transform child in _masksContainer)
         {
             _masks.Add(child.GetComponent<MaskBase>());
+            _masks[childCounter].SetTriangleCoords(_triangleCoordsElements);
+            _masks[childCounter].SetNeighbors(_leftNeighbor, _rightNeighbor, _upNeighbor);
             childCounter++;
         }
         _parentMasked = new Transform[_masks.Count];
     }
+
+    private void FindNeighbors(int n)
+    {
+
+
+        _leftNeighbor[n] = FindLeftNeighbor(n);
+        _rightNeighbor[n] = FindRightNeighbor(n);
+        _upNeighbor[n] = FindUpNeighbor(n);
+    }
+
+    private int FindLeftNeighbor(int n)
+    {
+
+        if (_triangleCoordsElements[n].y == 0)
+        {
+            //Debug.Log("no left");
+            return -1; //does not exist
+        }
+        else
+        {
+            return n - 1;
+        }
+    }
+
+    private int FindRightNeighbor(int n)
+    {
+        float numToCheck = n + 1;
+        bool lastInLine = Mathf.Sqrt(n + 1) % 1 == 0;
+
+        if (lastInLine)
+        {
+            //Debug.Log("no right");
+            return -1; //does not exist
+        }
+        else
+        {
+            return n + 1;
+        }
+    }
+
+    private int FindUpNeighbor(int n)
+    {
+        bool noUpneighbor = !((_leftNeighbor[n] >= 0) && (_rightNeighbor[n] >= 0));
+
+        bool a = _leftNeighbor[n] >= 0;
+        //if (!a) Debug.Log(_leftNeighbor[n] >= 0);
+        if (noUpneighbor)
+        {
+            //Debug.Log(n);
+            return -1; //does not exist
+        }
+        else
+        {
+            int valueToReturn;
+            valueToReturn = n - 2 * _triangleCoordsElements[n].x;
+            //THIS IS CORRECT
+            //ABOVE IS SHORTER
+            //Vector2Int posInTriangle = new Vector2Int(_triangleCoordsElements[n].x - 1, _triangleCoordsElements[n].y - 1);
+            //valueToReturn = posInTriangle.x * posInTriangle.x + posInTriangle.y;
+            return valueToReturn;
+        }
+    }
+
+    //private int FindDownNeighbor(int n)
+    //{NOT CORRECT!!!!
+    //    bool noDownneighbor = !((_leftNeighbor[n] >= 0) && (_rightNeighbor[n] >= 0));
+
+    //    bool a = _leftNeighbor[n] >= 0;
+    //    //if (!a) Debug.Log(_leftNeighbor[n] >= 0);
+    //    if (noDownneighbor)
+    //    {
+    //        Debug.Log(n);
+    //        return -1; //does not exist
+    //    }
+    //    else
+    //    {
+    //        int valueToReturn;
+    //        Vector2Int posInTriangle = new Vector2Int(_triangleCoordsElements[n].x - 1, _triangleCoordsElements[n].y - 1);
+    //        valueToReturn = posInTriangle.x * posInTriangle.x + posInTriangle.y;
+    //        return valueToReturn;
+    //    }
+    //}
+
 
     public void StartMasking()
     {
@@ -39,6 +135,7 @@ public class Masker : MonoBehaviour
         DuplicateCodeForMasking();
         for (int i = 0; i < _masks.Count; i++)
         {
+            //mipws 8a paei arga afto?
             _masks[i].MaskCode(_parentMasked[i]);
             _masks[i].CountScore(_parentMasked[i]);
         }
@@ -75,9 +172,14 @@ public class Masker : MonoBehaviour
     }
 
 
+
+
+
+
+
     //find row = square root of i floored down
     //find number in row = i - row^2
-   
+
 
 
 }
