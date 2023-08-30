8fb19fec4bf528011081d13298b8a1d85ddd6007
pevanget <84782645+pevanget@users.noreply.github.com>
2023-08-30 14:29:45 +0300

    Find end and size done

--- Trigonix/Assets/Scripts/ProcessScreenshot.cs
+++ Trigonix/Assets/Scripts/ProcessScreenshot.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using System.IO;
+using System;
 //C:\Users\THIS USER\AppData\LocalLow\DefaultCompany\My project
 public class ProcessScreenshot : MonoBehaviour
 {
@@ -10,6 +11,8 @@ public class ProcessScreenshot : MonoBehaviour
     //[SerializeField]
     private int _pixelStepBelow = 0;
     private int _pixelStepRight = 0;
+    private Vector2Int _startOfCode;
+    private Vector2Int _endOfCode;
     private Texture2D _tex;
     private Texture2D _texBnW;
     private string _pathForEditor;
@@ -20,16 +23,26 @@ public class ProcessScreenshot : MonoBehaviour
     {
         _pathForEditor = Application.dataPath + "/../Screenshots";
         _pathForBuild = Application.persistentDataPath;
-  
+
     }
 
     public void DecodeLastScreenshot()
     {
         LoadScreenshot();
         ThresholdScreenshot();
-        FindStartAndSize();
+        FindSpecs();
 
-        //DecodeScreenshot();
+        DecodeScreenshot();
+    }
+
+
+
+    private void DecodeScreenshot()
+    {
+        //for (int i = 0; i < length; i++)
+        //{
+
+        //}
     }
 
     private void LoadScreenshot()
@@ -75,20 +88,19 @@ public class ProcessScreenshot : MonoBehaviour
 
     }
 
-    private void FindStartAndSize()
+    private void FindSpecs()
     {
-        Vector2 startOfCode = new Vector2(-1,-1);
+        Vector2 startOfCode = new Vector2(-1, -1);
         Vector2 startOfBelowStart = new Vector2(-1, -1);
+        Vector2 endOfCode = new Vector2(-1, -1);
         bool succesful = false;
         if (File.Exists(_pathForEditor + "/SavedScreenBlackAndWhite.png"))
         {
             _fileData = File.ReadAllBytes(_pathForEditor + "/SavedScreenBlackAndWhite.png");
             _texBnW = new Texture2D(2, 2);
             _texBnW.LoadImage(_fileData);
-
-            startOfCode = FindStart();
-            startOfBelowStart = FindBelowStart(startOfCode);
             succesful = true;
+
             Debug.Log("BnW Screenshot found on editor");
 
         }
@@ -97,22 +109,49 @@ public class ProcessScreenshot : MonoBehaviour
             _fileData = File.ReadAllBytes(_pathForBuild + "/SavedScreenBlackAndWhite.png");
             _texBnW = new Texture2D(2, 2);
             _texBnW.LoadImage(_fileData);
-
-            startOfCode = FindStart();
-            startOfBelowStart = FindBelowStart(startOfCode);
             succesful = true;
+
             Debug.Log("BnW Screenshot found on build");
         }
         else Debug.Log("No BnW screenshot to process was found");
-        if (succesful)
-        {
-            _pixelStepBelow = (int)(startOfCode.y - startOfBelowStart.y);
-            Debug.Log(_pixelStepBelow);
-            //_pixelStepRight = 
-        }
+        startOfCode = FindStart();
+        startOfBelowStart = FindBelowStart(startOfCode);
+        endOfCode = FindEnd(startOfCode);
+        Debug.Log(endOfCode);
+
+
+        if (!succesful) return;
+
+        _pixelStepBelow = (int)(startOfCode.y - startOfBelowStart.y);
+        Debug.Log(_pixelStepBelow);
+        Vector2 endMiddleOfFirstTriangle = new Vector2(startOfCode.x, startOfCode.y - _pixelStepBelow);
+        _pixelStepRight = FindWidth(endMiddleOfFirstTriangle);
+        Debug.Log(_pixelStepRight);
+
+        Vector2 centerOfThirdTriangle = new Vector2(startOfCode.x, (int)(startOfCode.y - _pixelStepBelow * 1.5f));
+        Debug.Log(startOfCode);
+        Debug.Log(centerOfThirdTriangle);
+
 
     }
 
+    private int FindWidth(Vector2 centerOfBottomEdge)
+    {
+        int width = 0;
+        bool found = false;
+        int x = (int)centerOfBottomEdge.x;
+        int y = (int)centerOfBottomEdge.y;
+
+        while (!found)
+        {
+            x++;
+            Color col = _texBnW.GetPixel(x, y);
+            if (col == Color.white) found = true;
+        }
+        width = (x - 1 - (int)centerOfBottomEdge.x) * 2;
+
+        return width;
+    }
 
     private Vector2 FindStart()
     {
@@ -159,6 +198,24 @@ public class ProcessScreenshot : MonoBehaviour
         if (pos == new Vector2(-666, -666)) Debug.Log("Failed to find below");
         Debug.Log(pos);
         return pos;
+    }
+
+    private Vector2 FindEnd(Vector2 startCode)
+    {
+        int x = (int)startCode.x;
+        int y = (int)startCode.y;
+        int counter = 0;
+        bool found = false;
+        while (!found)
+        {
+            if ((_texBnW.GetPixel(x, counter)) == Color.black)
+            {
+                found = true;
+            }
+            counter++;
+        }
+        Vector2 end = new Vector2(x, counter - 1);
+        return end;
     }
 
 
