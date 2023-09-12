c9575f0966f7d81a5db3ab462a1fd42dbc78d0b4
pevanget <84782645+pevanget@users.noreply.github.com>
2023-09-12 19:24:28 +0300

    FINALLY

--- Trigonix/Assets/Scripts/ProcessScreenshot.cs
+++ Trigonix/Assets/Scripts/ProcessScreenshot.cs
@@ -14,11 +14,18 @@ public class ProcessScreenshot : MonoBehaviour
     private int _pixelStepRight = 0;
     private Vector2Int _startOfCode;
     private Vector2Int _endOfCode;
+    private int _linesCount;
     private Texture2D _tex;
     private Texture2D _texBnW;
+    private Texture2D _texBnWWithSamplingPoints;
     private string _pathForEditor;
     private string _pathForBuild;
     private byte[] _fileData; //might need to reset
+
+    private List<Vector2Int> _coordinatesOfLinesCentered = new List<Vector2Int>();
+    private List<Vector2Int> _coordinatesOfTriangles = new List<Vector2Int>();
+
+
     // Start is called before the first frame update
     void Start()
     {
@@ -31,14 +38,193 @@ public class ProcessScreenshot : MonoBehaviour
     {
         LoadScreenshot();
         ThresholdScreenshot();
-        
-        
-        FindSpecs();
 
-        DecodeScreenshot();
+
+        FindSpecs();
+        FindCoords();
+
+        //DecodeScreenshot();
     }
 
 
+    private void FindCoords()
+    {
+        FindCoordsOfLinesCentered();
+        FindCoordsInLines();
+    }
+
+    private void FindCoordsOfLinesCentered()
+    {
+        _texBnWWithSamplingPoints = _texBnW;
+        _coordinatesOfLinesCentered.Clear();
+
+        Vector2Int pos = new Vector2Int(-666, -666);
+        Vector2Int currentPosition = _startOfCode;
+        Vector2Int firstWhitePixelInTriangle = new Vector2Int(-1, -1);
+        Vector2Int lastWhitePixelInTriangle = new Vector2Int(-1, -1);
+        Vector2Int thisLineCoordsCentered = new Vector2Int(-1, -1);
+        bool alreadyFoundWhite = false;
+        int linesCount = 0;
+        if (_endOfCode.x < 0) //is this correct?
+        {
+            Debug.LogError("No valid screenshot found");
+            return;
+        }
+        while (currentPosition.y > _endOfCode.y)
+        {
+            if (_texBnW.GetPixel(currentPosition.x, currentPosition.y) == Color.black)
+            {
+                if (alreadyFoundWhite)
+                {
+                    lastWhitePixelInTriangle = new Vector2Int(currentPosition.x, currentPosition.y + 1);
+                    thisLineCoordsCentered = (firstWhitePixelInTriangle + lastWhitePixelInTriangle) / 2;
+                    _coordinatesOfLinesCentered.Add(thisLineCoordsCentered);
+                    _texBnWWithSamplingPoints.SetPixel(thisLineCoordsCentered.x, thisLineCoordsCentered.y, Color.blue);
+                    alreadyFoundWhite = false;
+
+                    linesCount++;
+                }
+            }
+            else
+            {
+                if (!alreadyFoundWhite)
+                {
+                    alreadyFoundWhite = true;
+                    firstWhitePixelInTriangle = currentPosition;
+                }
+            }
+            currentPosition = new Vector2Int(currentPosition.x, currentPosition.y - 1);
+
+        }
+        Debug.Log(linesCount);
+        _linesCount = linesCount;
+        byte[] bytes = _texBnWWithSamplingPoints.EncodeToPNG();
+        if (Directory.Exists(Application.dataPath + "/../Screenshots")) File.WriteAllBytes(Application.dataPath + "/../Screenshots/SavedScreenBlueDotsA.png", bytes);
+    }
+
+    private void FindCoordsInLines()
+    {
+        List<string> L = new List<string>(new string[10]);
+        int totalNumberOfElements = (_linesCount + 1) * (_linesCount + 1);
+        _coordinatesOfTriangles = new List<Vector2Int>(new Vector2Int[totalNumberOfElements]);
+        for (int currentLine = 0; currentLine < _linesCount; currentLine++)
+        {
+            FindCoordsInLine(currentLine);
+        }
+    }
+
+    private void FindCoordsInLine(int line)
+    {
+        int blackLinesToEncounter = line;
+        Vector2Int currentPosition = _coordinatesOfLinesCentered[line];
+        Vector2Int firstWhitePixelInTriangle = new Vector2Int(-1, -1);
+        Vector2Int lastWhitePixelInTriangle = new Vector2Int(-1, -1);
+        Vector2Int thisTriangleCoordsCentered = new Vector2Int(-1, -1);
+        int lineIndexStart = line * (line + 1); //0=0 1=2 2=6 3=12 4=20
+
+        Debug.Log(_coordinatesOfTriangles.Count);
+        _coordinatesOfTriangles[lineIndexStart] = currentPosition;
+        
+        //GO LEFT
+        bool alreadyFoundBlack = false;
+        bool alreadyFoundStart = false;
+
+        int countBlackLines = 0;
+        int countTriangles = 0;
+        while (blackLinesToEncounter>countBlackLines)
+        {
+            if (_texBnW.GetPixel(currentPosition.x, currentPosition.y) == Color.black)
+            {
+                if (!alreadyFoundBlack)
+                {
+                    alreadyFoundBlack = true;
+                }
+                else
+                {
+                    if (alreadyFoundStart)
+                    {
+                        lastWhitePixelInTriangle = new Vector2Int(currentPosition.x + 1, currentPosition.y);
+                        currentPosition = (firstWhitePixelInTriangle + lastWhitePixelInTriangle)/ 2;
+                        countTriangles++;
+
+                        int index = lineIndexStart - countTriangles;
+                        _coordinatesOfTriangles[index] = currentPosition;
+                        
+                        alreadyFoundStart = false;
+                        alreadyFoundBlack = false;
+
+
+                        _texBnWWithSamplingPoints.SetPixel(currentPosition.x, currentPosition.y, Color.blue);
+                        countBlackLines++;
+                    }
+                }
+            }
+            else
+            {
+                if((!alreadyFoundStart)&&(alreadyFoundBlack))
+                {
+                    firstWhitePixelInTriangle = currentPosition;
+                    alreadyFoundStart = true;
+                }
+            }
+            currentPosition = new Vector2Int(currentPosition.x-1, currentPosition.y);
+        }
+        byte[] bytes = _texBnWWithSamplingPoints.EncodeToPNG();
+        if (Directory.Exists(Application.dataPath + "/../Screenshots")) File.WriteAllBytes(Application.dataPath + "/../Screenshots/SavedScreenBlueDotsB.png", bytes);
+
+        //GO RIGHT
+         alreadyFoundBlack = false;
+         alreadyFoundStart = false;
+        currentPosition = _coordinatesOfLinesCentered[line];
+        countBlackLines = 0;
+         countTriangles = 0;
+        while (blackLinesToEncounter > countBlackLines)
+        {
+            if (_texBnW.GetPixel(currentPosition.x, currentPosition.y) == Color.black)
+            {
+                if (!alreadyFoundBlack)
+                {
+                    alreadyFoundBlack = true;
+                }
+                else
+                {
+                    if (alreadyFoundStart)
+                    {
+                        lastWhitePixelInTriangle = new Vector2Int(currentPosition.x - 1, currentPosition.y);
+                        currentPosition = (firstWhitePixelInTriangle + lastWhitePixelInTriangle) / 2;
+                        countTriangles++;
+
+                        int index = lineIndexStart + countTriangles;
+                        _coordinatesOfTriangles[index] = currentPosition;
+
+                        alreadyFoundStart = false;
+                        alreadyFoundBlack = false;
+
+
+                        _texBnWWithSamplingPoints.SetPixel(currentPosition.x, currentPosition.y, Color.blue);
+                        countBlackLines++;
+                    }
+                }
+            }
+            else
+            {
+                if ((!alreadyFoundStart) && (alreadyFoundBlack))
+                {
+                    firstWhitePixelInTriangle = currentPosition;
+                    alreadyFoundStart = true;
+                }
+            }
+            currentPosition = new Vector2Int(currentPosition.x + 1, currentPosition.y);
+        }
+        bytes = _texBnWWithSamplingPoints.EncodeToPNG();
+        if (Directory.Exists(Application.dataPath + "/../Screenshots")) File.WriteAllBytes(Application.dataPath + "/../Screenshots/SavedScreenBlueDotsB.png", bytes);
+
+    }
+
+    private void GoLeft()
+    {
+
+    }
 
     private void DecodeScreenshot()
     {
@@ -90,7 +276,7 @@ public class ProcessScreenshot : MonoBehaviour
 
     private void FindSpecs()
     {
-        
+
         Vector2 _startOfBelowStart = new Vector2(-1, -1);
         bool succesful = false;
         if (File.Exists(_pathForEditor + "/SavedScreenBlackAndWhite.png"))
@@ -216,7 +402,7 @@ public class ProcessScreenshot : MonoBehaviour
             }
             counter++;
         }
-        Vector2Int end = new Vector2Int (x, counter - 1);
+        Vector2Int end = new Vector2Int(x, counter - 1);
         return end;
     }
 
